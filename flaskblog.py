5fdf9e1491db82ba728d1c57e73c7303e13e8cc0
Ron Hu <codingmonkron@gmail.com>
2020-09-10 01:34:44 -0700

    a lot of new stuff

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,45 @@
-from flask import Flask
+from flask import Flask, render_template, url_for,flash,redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = 'c637300bb59271eb97d968c59086b5b0'
+
+posts = [
+	{
+		'author': 'Ron Hu',
+		'title': 'Blog Post 1',
+		'content': 'First post content',
+		'date_posted': 'October 27, 2000'
+	},
+	{
+		'author': 'Jane Doe',
+		'title': 'Blog Post 2',
+		'content': 'Second post content',
+		'date_posted': 'October 27, 2001'
+	}
+]
+
 @app.route("/")
-def hello():
-	return "<h1>Hello World!</h1>"
+@app.route("/home")
+def home():
+	return render_template('home.html', posts=posts)
+
+@app.route("/about")
+def about():
+	return render_template('about.html',title='About')
+
+@app.route("/register", methods=['GET','POST'])
+def register():
+	form  = RegistrationForm()
+	if form.validate_on_submit():
+		flash(f'Account created for {form.username.data}!','success')
+		return redirect(url_for('home'))
+	return render_template('register.html',title='Register', form=form)
+
+@app.route("/login")
+def login():
+	form  = LoginForm()
+	return render_template('login.html',title='Login',form=form)
+
+if __name__== '__main__':
+	app.run(debug=True)
